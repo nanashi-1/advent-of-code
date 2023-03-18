9783a32c797bd6fafd6d59944b5e9a5d11263f9b
nanashi-1 <bagaresalken@gmail.com>
2023-03-18 19:54:20 +0800

    move part 1 logic to a function

--- Day 1/index.ts
+++ Day 1/index.ts
@@ -1,24 +1,29 @@
 import input from './input';
 
-const splittedInput = input.split('\n\n'); // split the input by two new lines
-let largest = 0; // the calories that the elf with the largest amount of calories is carrying
+function part1() {
+	const splittedInput = input.split('\n\n'); // split the input by two new lines
+	let largest = 0; // the calories that the elf with the largest amount of calories is carrying
 
-/*
+	/*
     This will loop through all the elves and then loop through all their foods, then add all the calories they are carrying.
     
     This have a Big O notation of On^2, which is kind of bad, but I will try to improve this in the future.
 */
-for (const splittedSplittedInput of splittedInput) {
-	let sum = 0;
+	for (const splittedSplittedInput of splittedInput) {
+		let sum = 0;
 
-	for (const num of splittedSplittedInput.split('\n')) {
-		sum += Number.parseInt(num);
+		for (const num of splittedSplittedInput.split('\n')) {
+			sum += Number.parseInt(num);
+		}
+
+		if (sum > largest) largest = sum;
 	}
 
-	if (sum > largest) largest = sum;
+	return largest;
 }
 
+console.log('Part 1:');
 console.log(
 	'The elf carrying the largest amount of calories carries %d calories',
-	largest
+	part1()
 );
